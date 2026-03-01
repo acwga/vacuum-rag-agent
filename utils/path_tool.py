6e884010d6f894a38695177598c58382b32d9b3a
acwga <3074027753@qq.com>
2026-03-01 17:45:05 +0800

    路径工具

--- utils/path_tool.py
+++ utils/path_tool.py
@@ -6,6 +6,7 @@ import os
 def get_project_root():
     """
     获取工程所在的根目录
+    :return: 工程根目录的绝对路径
     """
     # 获取当前文件的绝对路径
     current_file = os.path.abspath(__file__)
@@ -14,4 +15,14 @@ def get_project_root():
 
     return project_root
 
-def get_abs_path(relative_path)
+def get_abs_path(relative_path):
+    """
+    将相对路径转换为绝对路径
+    :param relative_path: 相对路径
+    :return: 绝对路径
+    """
+    project_root = get_project_root()
+    return os.path.join(project_root, relative_path)
+
+if __name__ == "__main__":
+    print(get_abs_path('config/config.txt'))
