5e060eec97746f97e31a1b6fc59d8b5feb2f897c
acwga <3074027753@qq.com>
2026-03-02 13:37:58 +0800

    向量存储

--- utils/file_handler.py
+++ utils/file_handler.py
@@ -52,4 +52,4 @@ def pdf_loader(filepath: str, password: str = None) -> list[Document]:
     return PyPDFLoader(filepath, password).load()
 
 def txt_loader(filepath: str) -> list[Document]:
-    return TextLoader(filepath).load()
+    return TextLoader(filepath, encoding='utf-8').load()
